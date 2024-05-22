0b25c61fe40498c426062fa40d88b777d63649ba
mahmoud097 <74962993+mahmoud097@users.noreply.github.com>
2024-05-22 14:24:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+Features :
+Multiple difficulty levels
+Timer to track the duration of the game
+Score tracking based on performance
+User-friendly interface
+Sound effects for card flips and matches
+-------------------------------------
+Prerequisites :
+Visual Studio Code
+.NET Core SDK 
+-------------------------------------
+Clone the Repository : 
+git clone https://github.com/mahmoud097/Task-4-Memory-Scramble-Game
+-------------------------------------
+Play the Game :
+Open the generated executable and start playing the game
+-------------------------------------
+How to Play:
+-----------
+1- Start the Game : 
+Launch the game from the executable.
+Run it from Visual Studio Code.
+2- Select Difficulty Level : 
+Choose from different difficulty levels (Easy, Medium, Hard).
+3- Flip Cards : 
+Click on a card to flip it over.
+Remember the position and image of the card.
+4- Match Pairs : 
+Find and match pairs of cards by flipping them over. 
+If the cards match, they remain flipped. 
+If they do not match, they flip back over after a short delay.
+5- Complete the Game : 
+Match all pairs to complete the game.
+Your score and time taken will be displayed.
+----------------------------------------
+If you have any questions or feedback, feel free to reach out to the project maintainer at 
+mahmoudebrahim097@gmail.com 
+mohamed
+
---
+++ Task-4-Memory-Scramble-Game/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Task_4_Memory_Scramble_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            lblStatus = new Label();
+            lblTimtLeft = new Label();
+            btnRestart = new Button();
+            GameTimer = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // lblStatus
+            // 
+            lblStatus.AutoSize = true;
+            lblStatus.Location = new Point(334, 103);
+            lblStatus.Name = "lblStatus";
+            lblStatus.Size = new Size(136, 20);
+            lblStatus.TabIndex = 0;
+            lblStatus.Text = "Match or MisMatch";
+            // 
+            // lblTimtLeft
+            // 
+            lblTimtLeft.AutoSize = true;
+            lblTimtLeft.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblTimtLeft.Location = new Point(327, 162);
+            lblTimtLeft.Name = "lblTimtLeft";
+            lblTimtLeft.Size = new Size(143, 28);
+            lblTimtLeft.TabIndex = 1;
+            lblTimtLeft.Text = "Time Left : 30";
+            // 
+            // btnRestart
+            // 
+            btnRestart.BackColor = SystemColors.InactiveBorder;
+            btnRestart.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRestart.Location = new Point(327, 26);
+            btnRestart.Name = "btnRestart";
+            btnRestart.Size = new Size(143, 49);
+            btnRestart.TabIndex = 2;
+            btnRestart.Text = "Restart";
+            btnRestart.UseVisualStyleBackColor = false;
+            btnRestart.Click += RestartGameEvent;
+            // 
+            // GameTimer
+            // 
+            GameTimer.Interval = 1000;
+            GameTimer.Tick += TimerEvent;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(506, 367);
+            Controls.Add(btnRestart);
+            Controls.Add(lblTimtLeft);
+            Controls.Add(lblStatus);
+            Name = "Form1";
+            Text = "Memory-Scramble-Game";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblStatus;
+        private Label lblTimtLeft;
+        private Button btnRestart;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
