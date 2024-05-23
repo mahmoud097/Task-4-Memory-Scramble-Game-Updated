f4e25ab50fdb2cc7957961b9192931995f67ba1d
mahmoud097 <74962993+mahmoud097@users.noreply.github.com>
2024-05-24 00:01:47 +0300

    Update Form1.cs

--- Task-4-Memory-Scramble-Game/Form1.cs
+++ Task-4-Memory-Scramble-Game/Form1.cs
@@ -44,37 +44,10 @@ namespace Task_4_Memory_Scramble_Game
         }
         private void LoadPictures()
         {
-            int leftPos = 20;
-            int topPos = 20;
-            int rows = 0;
+           
 
-            for (int i = 0; i < 12; i++)
-            {
-                PictureBox newPic = new PictureBox();
-                newPic.Height = 50;
-                newPic.Width = 50;
-                newPic.BackColor = Color.LightGray;
-                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
-                newPic.Click += NewPic_Click;
-                pictures.Add(newPic);
-
-                if (rows < 3)
-                {
-                    rows++;
-                    newPic.Left = leftPos;
-                    newPic.Top = topPos;
-                    this.Controls.Add(newPic);
-                    leftPos = leftPos + 60;
-                }
-
-                if (rows == 3)
-                {
-                    leftPos = 20;
-                    topPos += 60;
-                    rows = 0;
-                }
-            }
-            RestartGame();
+               
+              
         }
 
         private void NewPic_Click(object? sender, EventArgs e)
@@ -112,56 +85,19 @@ namespace Task_4_Memory_Scramble_Game
 
         private void RestartGame()
         {
-            // randomise the original list
-            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
-            // assign the random list to the original
-            numbers = randomList;
-
-            for (int i = 0; i < pictures.Count; i++)
-            {
-                pictures[i].Image = null;
-                pictures[i].Tag = numbers[i].ToString();
-            }
-            tries = 0;
-            lblStatus.Text = "Mismatched: " + tries + "times.";
-            lblTimtLeft.Text = "Time Left: " + totalTime;
-            gameOver = false;
-            GameTimer.Start();
-            countDownTime = totalTime;
+            
 
         }
         private void CheckPictures(PictureBox A, PictureBox B)
         {
-            if (firstChoice == secondChoice)
-            {
-               
-            }
-            else
-            {
-                tries++;
-                lblStatus.Text = "Mismatched " + tries + " times.";
-            }
-            firstChoice = null;
-            secondChoice = null;
-
-            foreach (PictureBox pics in pictures.ToList())
-            {
-                if (pics.Tag != null)
-                {
-                    pics.Image = null;
-                }
-            }
+           
             // now lets check if all of the item have been solved 
-            if (pictures.All(o => o.Tag == pictures[0].Tag))
-            {
-                GameOver("Great Work, You Win!!!!!");
-            }
+            
+          
         }
         private void GameOver(string msg)
         {
-            GameTimer.Stop();
-            gameOver = true;
-            MessageBox.Show(msg + " Click Restart to Play Again. ", "Task 4 Says: ");
+            
         }
 
         private void Form1_Load(object sender, EventArgs e)
