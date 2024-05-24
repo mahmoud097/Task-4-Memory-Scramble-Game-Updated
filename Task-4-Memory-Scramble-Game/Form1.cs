322631de50facc7fa8fd4d553c0d3e2ae10c8dd1
Mohamed-el-feky <me3349210@gmail.com>
2024-05-24 09:29:55 +0300

    Update Form1.cs

--- Task-4-Memory-Scramble-Game/Form1.cs
+++ Task-4-Memory-Scramble-Game/Form1.cs
@@ -37,31 +37,64 @@ namespace Task_4_Memory_Scramble_Game
                 }
             }
         }
-
+             // Event handler to restart the game
+             
         private void RestartGameEvent(object sender, EventArgs e)
         {
             RestartGame();
         }
+
+        // Method to load the pictures into the PictureBox controls
+        
         private void LoadPictures()
         {
            int leftpos = 20;
            int toppos = 20;
            int rows = 0;
-
+           
+            // Create and configure PictureBox controls
+            
            for (int i = 0; I < 12; i++)
+           {
+                PictureBox newPic = new PictureBox();
+                newPic.Height = 50;
+                newPic.Width = 50;
+                newPic.BackColor = Color.LightGray;
+                newPic.SizeMode = PictureBoxSizeMode.StretchImage;
+                newPic.Click += NewPic_Click;
+                pictures.Add(newPic);
 
-               
-              
+                 if (rows < 3)
+                {
+                    rows++;
+                    newPic.Left = leftPos;
+                    newPic.Top = topPos;
+                    this.Controls.Add(newPic);
+                    leftPos = leftPos + 60;
+                }
+                if (rows == 3)
+                {
+                    leftPos = 20;
+                    topPos += 60;
+                    rows = 0;
+                }
+            }
+                 // Start a new game
+            RestartGame();
+  
         }
-
+              // Event handler for picture box click
+              
         private void NewPic_Click(object? sender, EventArgs e)
         {
             if (gameOver)
             {
                 // dont register a click if the game is over
+                
                 return;
             }
-
+                // Handle the first choice
+                
             if (firstChoice == null)
             {
                 picA = sender as PictureBox;
@@ -71,6 +104,9 @@ namespace Task_4_Memory_Scramble_Game
                     firstChoice = (string)picA.Tag;
                 }
             }
+
+            // Handle the second choice
+            
             else if (secondChoice == null)
             {
                 picB = sender as PictureBox;
@@ -82,26 +118,82 @@ namespace Task_4_Memory_Scramble_Game
                 }
             }
             else
-            {
+            { 
+            
+            // Check the pictures if both choices have been made
+            
                 CheckPictures(picA, picB);
             }
         }
 
-        private void RestartGame()
+              
+        private void RestartGame()    // Method to restart the game
         {
+             // Randomize the list of numbers
+
+             
+            var randomList = numbers.OrderBy(x => Guid.NewGuid()).ToList();
+            // assign the random list to the original
+            numbers = randomList;
+            for (int i = 0; i < pictures.Count; i++)
+            {
+
+            // Assign the random numbers to the PictureBox controls
+
+            
+                pictures[i].Image = null;
+                pictures[i].Tag = numbers[i].ToString();
+
+                
+            }    // Reset the game variables
+            
+            tries = 0;
+            lblStatus.Text = "Mismatched: " + tries + " times.";
+            lblTimeLeft.Text = "Time Left: " + totalTime;
+            gameOver = false;
+            GameTimer.Start();
+            countDownTime = totalTime;
             
 
         }
+          // Method to check the two chosen pictures
         private void CheckPictures(PictureBox A, PictureBox B)
         {
            
-            // now lets check if all of the item have been solved 
+           if (firstChoice == secondChoice)   // If the pictures match, set their tags to null
+            {
+                A.Tag = null;
+                B.Tag = null;
+            }
+            else
+            {
+                tries++;
+                lblStatus.Text = "Mismatched " + tries + " times.";  // If they do not match, increment the mismatch counter
+            }
+             
+            firstChoice = null;       // Reset the choices
+            secondChoice = null;
+            
+            foreach (PictureBox pics in pictures.ToList())    // Hide the pictures that do not match
+            {
+                if (pics.Tag != null)
+                {
+                    pics.Image = null;
+                }
+            }
+            // now lets check if all of the items have been solved
+            if (pictures.All(o => o.Tag == pictures[0].Tag))    // Check if all pairs have been matched
+            {
+                GameOver("Great Work, You Win!!!!");
+            }
             
           
         }
-        private void GameOver(string msg)
+        private void GameOver(string msg) // Method to handle the game over scenario
         {
-            
+            GameTimer.Stop();
+            gameOver = true;
+            MessageBox.Show(msg + " Click Restart to Play Again.", "Moo Says: ");
         }
 
         private void Form1_Load(object sender, EventArgs e)
